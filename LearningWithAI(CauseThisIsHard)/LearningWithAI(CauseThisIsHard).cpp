21431e7d0601a00be264a25d6cfeba3d848ea01b
Chase Cox <chasecox24@gmail.com>
2026-02-11 09:32:27 -0600

    Basic Framework for the game, also some cool inputs being read by using conio.h. This allows for use to use _kbhit() and _getch(). _kbhit() checs if a key was pressed and _getch() immediately reads the key.

--- LearningWithAI(CauseThisIsHard)/LearningWithAI(CauseThisIsHard).cpp
+++ LearningWithAI(CauseThisIsHard)/LearningWithAI(CauseThisIsHard).cpp
@@ -4,6 +4,8 @@
 // Inside the constructor, we initialize all the member variables.
 
 #include <iostream>
+#include <ctime>
+#include <conio.h>  // Windows-only for _kbhit() and _getch()
 
 class Character
 {
@@ -26,21 +28,45 @@ public:// everyone can have some
 		gold = gd;
 	}
 
+	void printStats()
+	{
+		std::cout << "Health: " << health << "/" << maxHealth
+			<< " | Armor: " << armor
+			<< " | Damage: " << damage
+			<< " | Gold: " << gold << std::endl;
+
+	}
+
+	void attack(Character &target) // Character &target, passes another character by reference so we can modify their stats directly.
+	{
+		int actualDamage = damage - target.armor; //function looks like warrior.attack(mage) this is passing a copy of the damage value from warrior into this funciton
+		if (actualDamage < 0) // prevents healing the enemy
+			actualDamage = 0;
+
+		target.health -= actualDamage;
+
+		if (target.health < 0)
+			target.health = 0; //just capping it
+
+		std::cout << "Attacked for " << actualDamage
+			<< " damage! Target health is now "
+			<< target.health << "/" << target.maxHealth << std::endl;
+	}
+
 	void takeDamage(int damageAmount)
 	{
-		if (armor > damageAmount)
-			return;
-		else
-		health = health - (damageAmount - armor);
-		if (health <= 0)
-			gold += 2;
+		int actualDamage = damageAmount - armor; // this int will check if real damage has accured
+		if (actualDamage < 0)
+			health -= actualDamage;
+
+		if (health <= 0) health = 0; // this clamps the health to 0
 	}
 
 	void healHealth(int healAmount)
 	{
 		health += healAmount;
 		if (health > maxHealth)
-				health = maxHealth;
+			health = maxHealth;
 	}
 };
 
@@ -65,7 +91,57 @@ public:
 	Archer() : Character(125, 30, 25, 15) {}
 };
 
+//MAIN
 int main()
 {
+	// Declaration
+	int choice;
+	char action;
+	int floor = 0;
+
+	Character* player = nullptr; //Character* allows us to store any subclass in one variable. This is polymorphism
+	choice;
+
+	std::cout << "Choose your class:\n";
+	std::cout << "1. Warrior\n2. Mage\n3. Archer\n";
+	std::cin >> choice;
+
+	//if else chain
+	if (choice == 1)
+		player = new Warrior();
+	else if (choice == 2)
+		player = new Mage();
+	else if (choice == 3)
+		player = new Archer();
+	else {
+		std::cout << "Invalid choice. Defaulting to Warrior.\n";
+		player = new Warrior();
+	}
+	std::cout << "\nClass chosen!\n";
+
+	std::cout << "Press 'w' to walk, 'q' to quit:\n";
+	while (true)
+	{
+		//since we handle input we do not need to tell the user invalid key entered // unless you want
+		//actions will happen immeditaily after key pressed feeling more responcive
+		if (_kbhit())  // check if a key was pressed
+		{
+			action = _getch();  // read key immediately
+
+			if (action == 'w')
+			{
+				floor++;
+				std::cout << "You walked to floor " << floor << std::endl;
+			}
+			else if (action == 'q')
+			{
+				std::cout << "Exiting game.\n";
+				break;
+			}
+		}
+	}
+
+
+	delete player; // frees the memory
 	return 0;
 }
